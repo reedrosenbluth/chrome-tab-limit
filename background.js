861cab3166e5f4d82a74c49f26196975d51ebe64
Reed Rosenbluth <331327+reedrosenbluth@users.noreply.github.com>
2022-02-25 11:54:10 -0500

    feat: work across multiple windows

--- background.js
+++ background.js
@@ -1,68 +1,67 @@
 var isEnabled = true;
-var maxTabs = 20;
+var maxTabs = 12;
 var tabsCount;
 
 function updateBadgeText() {
-    var tabsBalance = maxTabs - tabsCount;
-    var tabsAllowanceRemaining = (tabsBalance > 0) ? tabsBalance : 0;
+  var tabsBalance = maxTabs - tabsCount;
+  var tabsAllowanceRemaining = tabsBalance > 0 ? tabsBalance : 0;
 
-    chrome.browserAction.setBadgeText({
-        text: "" + tabsAllowanceRemaining
-    });
+  chrome.browserAction.setBadgeText({
+    text: "" + tabsAllowanceRemaining,
+  });
 }
 
 function updateTabsCount() {
-    chrome.tabs.query({
-        windowType: 'normal',
-        pinned: false
-    }, function (tabs) {
-        tabsCount = tabs.length;
-        updateBadgeText();
-    });
+  chrome.tabs.query(
+    {
+      currentWindow: true,
+      windowType: "normal",
+      pinned: false,
+    },
+    (tabs) => {
+      tabsCount = tabs.length;
+      updateBadgeText();
+    }
+  );
+}
+
+function updateTabsCountHandler(_arg) {
+  updateTabsCount();
 }
 
 function handleTabCreated(tab) {
-    if (tabsCount >= maxTabs) {
-        chrome.tabs.remove(tab.id);
-    }
-    else {
-        updateTabsCount();
-    }
-}
-
-function handleTabRemoved(tab) {
-    updateTabsCount();
-}
-
-function handleTabUpdated(tab) {
-    updateTabsCount();
+  updateTabsCount();
+  if (tabsCount >= maxTabs && tab.index > 0) {
+    chrome.tabs.remove(tab.id);
+  }
 }
 
 function init() {
-    updateTabsCount();
-    chrome.tabs.onCreated.addListener(handleTabCreated);
-    chrome.tabs.onRemoved.addListener(handleTabRemoved);
-    chrome.tabs.onUpdated.addListener(handleTabUpdated);
+  updateTabsCount();
+  chrome.windows.onFocusChanged.addListener(updateTabsCountHandler);
+  chrome.tabs.onCreated.addListener(handleTabCreated);
+  chrome.tabs.onRemoved.addListener(updateTabsCountHandler);
+  chrome.tabs.onUpdated.addListener(updateTabsCountHandler);
 }
 
 function teardown() {
-    chrome.tabs.onCreated.removeListener(handleTabCreated);
-    chrome.tabs.onRemoved.removeListener(handleTabRemoved);
-    chrome.tabs.onUpdated.removeListener(handleTabUpdated);
+  chrome.windows.onFocusChanged.removeListener(updateTabsCountHandler);
+  chrome.tabs.onCreated.removeListener(handleTabCreated);
+  chrome.tabs.onRemoved.removeListener(updateTabsCountHandler);
+  chrome.tabs.onUpdated.removeListener(updateTabsCountHandler);
 }
 
 chrome.browserAction.onClicked.addListener(function (tab) {
-    if (!isEnabled) {
-        init();
-        chrome.browserAction.setIcon({ path: "icons/19.png" });
-    }
-    else {
-        teardown();
-        chrome.browserAction.setIcon({ path: "icons/19-disabled.png" });
-        chrome.browserAction.setBadgeText({ 'text': '' });
-    }
+  if (!isEnabled) {
+    init();
+    chrome.browserAction.setIcon({ path: "icons/19.png" });
+  } else {
+    teardown();
+    chrome.browserAction.setIcon({ path: "icons/19-disabled.png" });
+    chrome.browserAction.setBadgeText({ text: "" });
+  }
 
-    isEnabled = !isEnabled;
+  isEnabled = !isEnabled;
 });
 
 init();
